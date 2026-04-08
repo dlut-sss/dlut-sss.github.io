68be2f28b40dcd5e8f935997743b5878c1ec89c9
Saurlax <saurlax@qq.com>
2026-04-08 15:27:22 +0800

    feat: update to astro v6

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,9 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection } from "astro:content";
+import { glob } from "astro/loaders";
+import { z } from "astro/zod";
 
-// 3. 定义你的集合
 const blog = defineCollection({
+  loader: glob({ pattern: "**/[^_]*.{md,mdx}", base: "./src/content/blog" }),
   schema: z.object({
     title: z.string(),
     date: z.date(),
