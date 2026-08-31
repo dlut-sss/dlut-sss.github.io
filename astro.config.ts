969a23bb4054d793ba08d55e45b2a3ebcb3e3706
Saurlax <saurlax@qq.com>
2026-08-31 18:23:06 +0800

    chore: upgrade Astro to v7

--- astro.config.ts
+++ astro.config.ts
@@ -1,15 +1,19 @@
 import { defineConfig } from "astro/config";
-import UnoCSS from "unocss/astro";
+import { unified } from "@astrojs/markdown-remark";
+import UnoCSS from "@unocss/astro";
 import remarkToc from "remark-toc";
 
 export default defineConfig({
   site: "https://dlut-sss.github.io",
+  compressHTML: true,
   integrations: [
     UnoCSS({
       injectReset: true,
     }),
   ],
   markdown: {
-    remarkPlugins: [[remarkToc, { heading: "目录", maxDepth: 3 }]],
+    processor: unified({
+      remarkPlugins: [[remarkToc, { heading: "目录", maxDepth: 3 }]],
+    }),
   },
 });
